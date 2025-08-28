1ad3795c902fb258a1c6bcea2e7d35e18ac0f118
bopry <bopry6@gmail.com>
2025-08-28 14:11:05 +0300

    Update tracks.js

--- tracks.js
+++ tracks.js
@@ -1,1 +1,1 @@
-const trackNamesArr = [{trackName:"dj_osama_platina", imgName:"dj_osama_platina"}];
+const trackNamesArr = [{trackName:"dj_osama_platina", imgName:"dj_osama_platina.png"}, {trackName:"YUNG TRAPPA – Розовое вино 2", imgName:"IMG_1631.jpeg"}];
